a810df0f781c36b2dadb79a12a898079cc2dc746
ArtArtur <arturgfs@hotmail.com>
2022-12-10 02:32:01 -0300

    Initial Commit

--- src/components/telas/TelaInicial.jsx
+++ src/components/telas/TelaInicial.jsx
@@ -8,15 +8,32 @@ export default function TelaInicial() {
   return (
     <>
       <ContainerCadastro>
+      <div className="content">
+          <h4>
+            A educação profissional transcende a finalidade de estrita
+            preparação de mão de obra, sendo, inclusive, um fator de
+            desenvolvimento humano e inclusão social, cultural e produtiva.
+            Desta forma, além da formação profissional, o Senac, através da
+            COPEG, favorece a inserção dos egressos dos seus cursos no mercado
+            de trabalho e verifica a qualidade percebida pelo mercado dos
+            egressos qualificados.
+          </h4>
+        </div>
         <div className="div-button">
-          <button 
-           onClick={() => {
-            navigate("/form");
-          }}>Responder Formulário</button>
           <button
-           onClick={() => {
-            navigate("/");
-          }}>Logout</button>
+            onClick={() => {
+              navigate("/form");
+            }}
+          >
+            Responder Formulário
+          </button>
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Logout
+          </button>
         </div>
       </ContainerCadastro>
     </>
